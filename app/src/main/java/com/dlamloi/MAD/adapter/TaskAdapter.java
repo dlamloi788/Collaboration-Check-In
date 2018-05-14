6e4ebf4ab9fae665789d3095c164575be7d2d52e
Don <Don.L.Loi@student.uts.edu.au>
2018-05-14 12:52:20 +1000

    Created Task Adapter, beginning to work on Tasks functionality

---
+++ app/src/main/java/com/dlamloi/MAD/adapter/TaskAdapter.java
@@ -0,0 +1,85 @@
+package com.dlamloi.MAD.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dlamloi.MAD.R;
+import com.dlamloi.MAD.model.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+import ViewHolder.TaskViewHolder;
+
+/**
+ * Created by Don on 14/05/2018.
+ */
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+
+    private ArrayList<Task> mTasks = new ArrayList<>();
+    private Context mContext;
+    private DatabaseReference mDatabaseReference;
+
+
+    public TaskAdapter(Context context, DatabaseReference databaseReference) {
+        this.mContext = context;
+        this.mDatabaseReference = databaseReference;
+
+        mDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Task task = dataSnapshot.getValue(Task.class);
+                mTasks.add(task);
+                notifyItemInserted(mTasks.size());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.fragment_task, parent, false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
