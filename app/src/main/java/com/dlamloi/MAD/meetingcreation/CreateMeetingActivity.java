af49ee169119c9885b4ec9f1f39a6bddfc3e416e
Don <Don.L.Loi@student.uts.edu.au>
2018-06-02 16:41:20 +1000

    All functions added, moving onto comments now

--- app/src/main/java/com/dlamloi/MAD/meetingcreation/CreateMeetingActivity.java
+++ app/src/main/java/com/dlamloi/MAD/meetingcreation/CreateMeetingActivity.java
@@ -2,7 +2,6 @@ package com.dlamloi.MAD.meetingcreation;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
@@ -12,13 +11,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.dlamloi.MAD.R;
+import com.dlamloi.MAD.groupcreation.CreateGroupContract;
+import com.dlamloi.MAD.groupcreation.CreateGroupPresenter;
 import com.dlamloi.MAD.home.GroupHomeActivity;
+import com.dlamloi.MAD.taskcreation.CreateTaskContract;
+import com.dlamloi.MAD.utilities.Utility;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
@@ -29,12 +30,15 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
+/**
+ * This class is reponsible for showing the create meeting activity UI
+ */
 public class CreateMeetingActivity extends AppCompatActivity implements CreateMeetingContract.View {
 
     public static final int PLACE_PICKER_REQUEST = 1;
 
     private String mGroupId;
-    private CreateMeetingPresenter mCreateMeetingPresenter;
+    private CreateMeetingContract.Presenter mCreateMeetingPresenter;
 
     @BindView(R.id.meeting_name_edittext)
     EditText mMeetingNameEt;
@@ -50,7 +54,12 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
     private Menu mMenu;
     private BottomSheetDialog mBottomSheetDialog;
     private Intent placePickerIntent;
+    private AlertDialog mLeaveAlertDialog;
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meeting);
@@ -60,38 +69,54 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(getDrawable(R.drawable.cross_icon));
         ButterKnife.bind(this);
+        setUpPlacePickerIntent();
         mGroupId = getIntent().getStringExtra(GroupHomeActivity.GROUP_KEY);
         mCreateMeetingPresenter = new CreateMeetingPresenter(this, mGroupId);
-        setUpPlacePickerIntent();
         setUpBottomSheetDialog();
+        mLeaveAlertDialog = Utility.setUpLeaveAlertDialog(this, getString(R.string.quit_scheduling_meeting));
     }
 
+    /**
+     * Sets up place picker intent
+     */
     private void setUpPlacePickerIntent() {
         try {
-            placePickerIntent = new PlacePicker.IntentBuilder().build(this);
+            placePickerIntent = new PlacePicker.IntentBuilder().build(CreateMeetingActivity.this);
         } catch (GooglePlayServicesRepairableException e) {
             placePickerIntent = null;
         } catch (GooglePlayServicesNotAvailableException e) {
             placePickerIntent = null;
         }
-
     }
 
+    /**
+     * Opens google picker map when the meeting location edittext has been tapped
+     */
     @OnClick(R.id.meeting_location_edittext)
     public void meetingLocationEtClicked() {
         mCreateMeetingPresenter.selectLocation(mMeetingLocationEt.getText().toString());
     }
 
+    /**
+     * Opens the date dialog when the meeting date edittext is tapped on
+     */
     @OnClick(R.id.meeting_date_edittext)
     public void meetingDateEtClicked() {
         mCreateMeetingPresenter.meetingDateEtClicked();
     }
 
+    /**
+     * Opens the time dialog when the meeting time edittext is tapped on
+     */
     @OnClick(R.id.meeting_time_edittext)
     public void meetingTimeEtClicked() {
         mCreateMeetingPresenter.meetingTimeClick();
     }
 
+    /**
+     * Determines whether the create menu button should be enabled, if all text is empty disabled
+     * otherwise enabled
+     */
     @OnTextChanged(value = {R.id.meeting_name_edittext, R.id.meeting_date_edittext,
             R.id.meeting_time_edittext, R.id.meeting_location_edittext, R.id.meeting_agenda_edittext},
             callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
@@ -104,7 +129,9 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         );
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.create_meeting_menu, menu);
@@ -112,12 +139,18 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         return super.onCreateOptionsMenu(menu);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                onBackPressed();
+                mCreateMeetingPresenter.homeButtonPressed(mMeetingNameEt.getText().toString(),
+                        mMeetingDateEt.getText().toString(),
+                        mMeetingTimeEt.getText().toString(),
+                        mMeetingLocationEt.getText().toString(),
+                        mMeetingAgendaEt.getText().toString());
                 break;
 
             case R.id.create_meeting_menu_button:
@@ -135,17 +168,26 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mCreateMeetingPresenter.result(requestCode, resultCode, data);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setPlaceText(Intent data) {
         Place place = PlacePicker.getPlace(this, data);
         mMeetingLocationEt.setText(place.getAddress());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showDateDialog(int currentYear, int currentMonth, int currentDayOfMonth) {
 
@@ -157,23 +199,17 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         datePickerDialog.show();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setMeetingDate(String date) {
         mMeetingDateEt.setText(date);
     }
 
-    @Override
-    public void onBackPressed() {
-        AlertDialog leaveConfirmationDialog = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_Alert).create();
-        mCreateMeetingPresenter.onBackPressed(leaveConfirmationDialog,
-                mMeetingNameEt.getText().toString(),
-                mMeetingDateEt.getText().toString(),
-                mMeetingTimeEt.getText().toString(),
-                mMeetingLocationEt.getText().toString(),
-                mMeetingAgendaEt.getText().toString()
-        );
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showTimeDialog(int currentHourOfDay, int currentMinute) {
 
@@ -184,62 +220,62 @@ public class CreateMeetingActivity extends AppCompatActivity implements CreateMe
         timePickerDialog.show();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setTimeText(String time) {
+    public void setMeetingTime(String time) {
         mMeetingTimeEt.setText(time);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void enableCreateButton() {
         mMenu.findItem(R.id.create_meeting_menu_button).setEnabled(true);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void disableCreateButton() {
         mMenu.findItem(R.id.create_meeting_menu_button).setEnabled(false);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showBottomSheetDialog() {
         mBottomSheetDialog.show();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void startSelectLocation() {
         startActivityForResult(placePickerIntent, PLACE_PICKER_REQUEST);
     }
 
-    @Override
-    public void meetingPublished() {
-        finish();
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void leave() {
         finish();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void showLeaveConfirmationDialog(AlertDialog leaveConfirmationDialog) {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
-        leaveConfirmationDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        leaveConfirmationDialog.setCancelable(false);
-        leaveConfirmationDialog.setMessage(getString(R.string.quit_scheduling_meeting));
-
-        leaveConfirmationDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.yes),
-                (dialog, which) -> finish());
-
-        leaveConfirmationDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.no),
-                (dialog, which) -> mCreateMeetingPresenter.leaveDialogNoClick(leaveConfirmationDialog));
-
-        leaveConfirmationDialog.show();
+    public void showLeaveConfirmationDialog() {
+        mLeaveAlertDialog.show();
     }
 
-    @Override
-    public void hideLeaveDialog(AlertDialog leaveConfirmationDialog) {
-        leaveConfirmationDialog.dismiss();
-    }
 
     /**
      * Opens up a menu at the bottom of the screen if the user attempts to
